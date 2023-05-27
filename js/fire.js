110e328781c210f5e1c4b5dc18f6b35b987331cb
Sameera <sameerawncd@gmail.com>
2023-05-27 15:18:47 +0530

    finalized mini game

---
+++ js/fire.js
@@ -0,0 +1,172 @@
+class Banana {
+    dy = Math.abs(Math.random() * 10);
+    width = 40;
+    height = this.width;
+    y = 0;
+    x = Math.random() * (innerWidth - this.width);
+    elm;
+    
+    constructor(){
+        this.elm = document.createElement('div');
+        this.elm.style.position = 'absolute';
+        this.elm.style.width = `${this.width}px`;
+        this.elm.style.height = `${this.height}px`;
+        this.elm.style.backgroundImage = `url('img/assets/banana.png')`;
+        this.elm.style.backgroundSize = 'cover';
+        this.elm.style.backgroundRepeat = 'no-repeat';
+        this.elm.style.backgroundPosition = 'center';
+        this.elm.style.left = `${this.x}px`;
+        this.elm.style.top = `${this.y}px`;
+        document.body.append(this.elm);
+    }
+
+    move(){
+        this.y += this.dy;
+        this.x = this.x;
+        this.elm.style.top = `${this.y}px`;
+        this.elm.style.left = `${this.x}px`;
+
+        if (this.y >= innerHeight - this.height){
+            this.y = 0;
+            this.x = Math.random() * (innerWidth - this.width);
+        }
+
+        if (this.y > boxElm.offsetTop && this.y < boxElm.offsetTop + 150 && this.x > boxElm.offsetLeft && this.x < boxElm.offsetLeft + 150){
+            bananaCount++;
+            score.innerHTML = `Score: ${bananaCount}`;
+            this.y = 0;
+            this.x = Math.random() * (innerWidth - this.width);
+        }
+    }
+}
+
+class Fire {
+    width = 40;
+    height = this.width;
+    x = Math.random() < 0.5 ? -this.width : innerWidth; 
+    y = 698; 
+    dx = Math.random() < 0.5 ? 2 : -2; 
+    elm;
+
+    constructor() {
+        this.elm = document.createElement('div');
+        this.elm.style.position = 'absolute';
+        this.elm.style.width = `${this.width}px`;
+        this.elm.style.height = `${this.height}px`;
+        this.elm.style.backgroundImage = `url('img/assets/fire.gif')`;
+        this.elm.style.backgroundSize = 'cover';
+        this.elm.style.backgroundRepeat = 'no-repeat';
+        this.elm.style.backgroundPosition = 'center';
+        this.elm.style.left = `${this.x}px`;
+        this.elm.style.top = `${this.y}px`;
+        document.body.append(this.elm);
+    }
+
+    move() {
+        this.x += this.dx;
+        this.elm.style.left = `${this.x}px`;
+        if ((this.dx > 0 && this.x > innerWidth) || (this.dx < 0 && this.x < -this.width)) {
+            this.elm.remove();
+            fireList = fireList.filter((fire) => fire !== this);
+        }
+
+        if ((this.x === boxElm.offsetLeft + boxElm.offsetWidth/2 - 15 || this.x === boxElm.offsetLeft + boxElm.offsetWidth/2 + 20) && boxElm.offsetTop + boxElm.offsetHeight >= this.y){
+            clearInterval(timer1);
+            timer1 = null;
+            clearInterval(timer2);
+            timer2 = null;
+            clearInterval(timer3);
+            timer3 = null;
+            clearInterval(initialTimer1);
+            initialTimer1 = null;
+            clearInterval(initialTimer2);
+            initialTimer2 = null;
+            clearInterval(initialTimer3);
+            initialTimer3 = null;
+            doDead();
+            
+            gameOverWindow.classList.remove("animate__backOutLeft");
+            gameOverWindow.style.visibility = 'visible';
+            finalScore.innerText = `Your Score: ${bananaCount}`;
+        }
+    }
+}
+
+let bananaCount = 0;
+let score = document.getElementById("score");
+const gameOverWindow = document.getElementById("game-over-window");
+const btnPlay = document.getElementById("btn-play");
+const finalScore = document.getElementById("final-score");
+
+let bananaList = [];
+for(let i = 1; i < 5; i ++){
+    bananaList.push(new Banana());
+}
+
+let fireList = [];
+let timer1 = null;
+let timer2 = null;
+let timer3 = null;
+let deadTimer = null;
+
+function doDead(){
+    let i = 1;
+    boxElm.style.width = 220 + "px";
+    deadTimer = setInterval(()=> {
+        boxElm.style.backgroundImage = `url('img/Dead (${i++}).png')`;
+        if(i === 9) {
+            i = 1;
+            clearInterval(deadTimer);
+            deadTimer = null;
+        }
+    },(1000/20));
+}
+  
+btnPlay.addEventListener("click", ()=> {
+    gameOverWindow.classList.add("animate__backOutLeft");
+    score.innerHTML = `Score: 0`;
+    bananaCount = 0;
+    resetGame();
+    startGame();
+    moveParticles();
+    
+});
+
+function moveParticles(){
+
+    timer1 = setInterval(()=>{bananaList.forEach(elm => elm.move())},20);
+
+    timer2 = setInterval(() => {
+        fireList.push(new Fire());
+    }, 4000);
+
+    timer3 = setInterval(() => {
+        fireList.forEach((fire) => fire.move());
+    }, 10);
+}
+
+function resetGame() {
+    bananaList.forEach(banana => {
+        banana.y = 0;
+        banana.x = Math.random() * (innerWidth - banana.width);
+        banana.elm.style.top = `${banana.y}px`;
+        banana.elm.style.left = `${banana.x}px`;
+    });
+
+   
+    fireList.forEach(fire => fire.elm.remove());
+    fireList = [];
+
+    clearInterval(timer1);
+    clearInterval(timer2);
+    clearInterval(timer3);
+    clearInterval(initialTimer1);
+    clearInterval(initialTimer2);
+    clearInterval(initialTimer3);
+    clearInterval(deadTimer);
+    
+    gameOverWindow.style.visibility = 'hidden';
+}
+
+moveParticles();
+
---
+++ js/main.js
@@ -0,0 +1,181 @@
+for(let i = 1; i <= 12; i++){
+    const image = new Image();
+    image.src  = `img/Jump (${i}).png`;
+}
+for(let i = 1; i <= 10; i++){
+    const image = new Image();
+    image.src  = `img/Idle (${i}).png`;
+}
+for(let i = 1; i <= 10; i++){
+    const image = new Image();
+    image.src  = `img/Walk (${i}).png`;
+}
+for(let i = 1; i <= 8; i++){
+    const image = new Image();
+    image.src  = `img/Run (${i}).png`;
+}
+
+
+const boxElm = document.createElement('div');
+boxElm.classList.add('box');
+document.body.append(boxElm);
+
+let initialTimer1 = null;
+let initialTimer2 = null;
+let initialTimer3 = null;
+
+let jump = false;
+let run = false;
+let runFast = false;
+let dx = 0;
+let runRight = false;
+let runLeft = false;
+
+function startGame(){
+    boxElm.style.left = window.innerWidth/2 - 70 + "px";
+    
+    document.body.addEventListener('keydown', (eventData)=>{
+        if (eventData.code === 'Space'){
+            jump = true;
+        } else if (eventData.code === 'ArrowRight' || eventData.code === 'KeyD'){
+            // run = true;
+            runRight = true;
+            boxElm.style.transform = 'rotateY(0deg)';
+            dx = 2;
+        } else if (eventData.code === 'ArrowLeft' || eventData.code === 'KeyA'){
+            // run = true;
+            runLeft = true;
+            boxElm.style.transform = `rotateY(180deg)`;
+            dx = -2;
+        } else if (eventData.code === 'ShiftLeft'){
+            runFast = true;
+            
+        }    
+    
+    });
+    
+    
+    document.body.addEventListener('keyup', (eventData)=>{
+        if (eventData.code === 'ArrowRight' || eventData.code === 'KeyD' || eventData.code === 'ArrowLeft' || eventData.code === 'KeyA'){
+            runRight = false;
+            runLeft = false;
+            dx = 0
+        } else if (eventData.code === 'ShiftLeft'){
+            runFast = false;
+            dx = 0;
+        }
+    });
+
+    initialTimer1 = setInterval(()=>{
+        if (runRight && !runFast){
+            dx = 2;
+            doRun();
+        }
+        if (runRight && runFast){
+            dx = 4;
+            doRunFast();
+        }
+        if (runLeft && !runFast){
+            dx = -2;
+            doRun();
+        }
+        if (runLeft && runFast){
+            dx = -4;
+            doRunFast();
+        }
+    },5);
+    
+    initialTimer2 = setInterval(()=>{
+        if (jump){
+            doJump();
+        }
+    }, 30);
+    
+    initialTimer3 = setInterval(()=> 
+    {
+        if (runRight && !jump && !runFast){
+            doRunImg();
+        }
+        if (runRight && runFast && !jump && !runLeft){
+            doRunFastImg();
+        }
+        if (runLeft && !jump && !runFast){
+            doRunImg();
+        }
+        if (runLeft && runFast && !jump && !runRight){
+            doRunFastImg();
+        }
+        if (jump){
+            dojumpImg();
+        }
+        if (!runRight && !jump && !runLeft){
+            drawIdle();
+        }
+        
+    }
+    , (1000/20));
+}
+
+let s = 0;
+let t = 0;
+let u = 2.5; 
+function doJump(){
+    t++;
+    let v = (u + (-0.2 * t));
+    s = (u * t) + (-0.1 * t * t);
+    s *=2;
+    if (v > 0){
+        boxElm.style.top = boxElm.offsetTop - s + "px";
+    } else if (v < 0){
+        boxElm.style.top = boxElm.offsetTop + s + "px";
+    }   
+    if (s <= 0){
+        jump = false;
+        t = 0; 
+    } 
+}
+
+function doRun(){
+    let x = boxElm.offsetLeft + dx;
+    if ((x + boxElm.offsetWidth)> innerWidth) {
+        x = innerWidth - boxElm.offsetWidth;
+    }else if (x <= 0) x = 0;
+    boxElm.style.left = `${x}px`;
+   
+}
+
+function doRunFast(){
+    let x = boxElm.offsetLeft + dx;
+    if ((x + boxElm.offsetWidth)> innerWidth) {
+        x = innerWidth - boxElm.offsetWidth;
+    }else if (x <= 0) x = 0;
+    boxElm.style.left = `${x}px`;
+}
+
+let i = 1;
+function drawIdle(){
+    boxElm.style.backgroundImage = `url('img/Idle (${i++}).png')`;
+    if(i === 11) i = 1;
+}
+
+let j = 1;
+function dojumpImg(){
+    boxElm.style.backgroundImage = `url('img/Jump (${j++}).png')`;
+    if (j === 13) j = 1;
+}
+
+let k = 1;
+function doRunImg(){
+    boxElm.style.backgroundImage = `url('img/Walk (${k++}).png')`;
+    if (k === 11) k = 1;
+}
+
+let l = 1;
+function doRunFastImg(){
+    boxElm.style.backgroundImage = `url('img/Run (${l++}).png')`;
+    if (l === 9) l = 1;
+}
+
+startGame();
+
+
